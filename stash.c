89eef306a7fce8bafb9048ed123c0c75401e32d9
Anton <antonkuprik2007@gmail.com>
2026-07-24 16:43:43 +0400

    fix: fixed memory leaks and error handling

--- stash.c
+++ stash.c
@@ -4,10 +4,8 @@
 #include <unistd.h>
 
 #define MAX_INPUT 1024
-
 /*
- * @brief Function prints current directory
- * @return writes current directory into buffer
+ * @brief Function writes current directory into buffer
  * */
 char *get_cwd(char *buf, size_t bufsize) {
   if (getcwd(buf, bufsize) == NULL) {
@@ -17,23 +15,41 @@ char *get_cwd(char *buf, size_t bufsize) {
   return buf;
 }
 
+int parse_args() { return 0; }
+
 /*
  * @brief Function to take input from user
+ *
+ * @note Dont forget to free() buffer
  * */
-void shell_input(void) {
+char *shell_input(void) {
   char *buf = NULL;
   size_t bufsize = 0;
   char cwd[MAX_INPUT];
 
-  printf("%s $> ", get_cwd(cwd, sizeof(cwd)));
-
-  fflush(stdout);
-  if (getline(&buf, &bufsize, stdin) == -1) {
+  if (get_cwd(cwd, sizeof(cwd)) == NULL) {
+    snprintf(cwd, sizeof(cwd), "?");
   }
+  printf("%s $> ", cwd);
+  fflush(stdout); // Reset buffer
 
-  printf("Input %s", buf);
-
-  free(buf);
+  if (getline(&buf, &bufsize, stdin) == -1) {
+    free(buf);
+    // Newline if CTRL+D or EOF
+    if (feof(stdin)) {
+      printf("\n");
+    } else {
+      perror("getline");
+    }
+    return NULL;
+  }
+  //
+  buf[strcspn(buf, "\n")] = '\0';
+  return buf;
 }
 
-int main(void) { shell_input(); }
+int main(void) {
+  char *line = shell_input();
+
+  free(line);
+}
