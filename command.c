16607092b6ae0cc0c6defb5a29cf7da7c3281c92
Anton <antonkuprik2007@gmail.com>
2026-08-08 16:03:19 +0400

    feat: pipeline parsing function

--- command.c
+++ command.c
@@ -4,8 +4,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+ *
+ *
+ * */
+
 char **tokenize(char *line, int *num_tokens) {
   char **tokens = malloc(MAX_ARGS * sizeof(*tokens));
+  if (tokens == NULL) {
+    perror("malloc");
+    return NULL;
+  }
   unsigned int position = 0;
 
   char *token = strtok(line, DELIMITER);
@@ -22,6 +31,9 @@ char **tokenize(char *line, int *num_tokens) {
 }
 
 Command *parse_tokens(char **tokens, int num_tokens) {
+  if (num_tokens <= 0) {
+    fprintf(stderr, "error: empty command\n");
+  }
   Command *command = malloc(sizeof(Command));
   if (command == NULL) {
     perror("malloc");
@@ -89,4 +101,58 @@ Command *parse_tokens(char **tokens, int num_tokens) {
   return command;
 }
 
-int execute(Command *cmd) { return 0; }
+Command *parse_pipeline(char *line) {
+  Command *head = NULL;
+  Command *curr = NULL;
+
+  char *curr_str = line;
+  char *next_str = NULL;
+
+  int num_tokens = 0;
+
+  while (curr_str != NULL) {
+    char *pipe_position = strchr(curr_str, '|');
+
+    if (pipe_position != NULL) {
+      *pipe_position = '\0';
+      pipe_position++;
+
+      next_str = pipe_position;
+
+    } else {
+      next_str = NULL;
+    }
+
+    char **tokens = tokenize(curr_str, &num_tokens);
+    Command *command = parse_tokens(tokens, num_tokens);
+
+    free(tokens);
+
+    if (command == NULL) {
+      free_pipeline(head);
+      return NULL;
+    }
+
+    if (head == NULL) {
+      head = command;
+      curr = head;
+    } else {
+      curr->next = command;
+      curr = command;
+    }
+
+    curr_str = next_str;
+    if (next_str == NULL)
+      break;
+  }
+  return head;
+}
+
+void free_pipeline(Command *head) {
+  while (head != NULL) {
+    Command *next = head->next;
+    free(head->argv);
+    free(head);
+    head = next;
+  }
+}
