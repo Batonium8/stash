c32392a1b5deceda84f1e8a854ce278ea2fe893b
Anton <antonkuprik2007@gmail.com>
2026-08-08 15:23:44 +0400

    feat: tokens parsing function

--- command.h
+++ command.h
@@ -1,9 +1,15 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
 #define MAX_INPUT 1024
 #define MAX_ARGS 64
 #define DELIMITER " "
 
 typedef struct command {
   char **argv;
+  char *infile;
+  char *outfile;
+  int append;
   struct command *next;
 } Command;
 
@@ -11,4 +17,10 @@ char **tokenize(char *line, int *num_tokens);
 
 Command *parse_tokens(char **tokens, int num_tokens);
 
-int execute(Command *cmd);
+Command *parse_pipeline(char *line);
+
+int execute_pipeline(Command *head);
+
+void free_pipeline(Command *head);
+
+#endif
